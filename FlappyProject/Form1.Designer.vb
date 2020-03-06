d50c1cb38a525cd17d39dc335601435142027209
gustavocano23 <gustavo00inmare@gmail.com>
2020-03-05 19:36:27 -0600

    Primer Commit

---
+++ FlappyProject/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 89)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(34, 24)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(-2, 308)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(385, 60)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(7, 326)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Score:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(51, 326)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(365, 367)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ FlappyProject/Form1.vb
@@ -0,0 +1,110 @@
+﻿Public Class Form1
+    Dim yspeed As Integer = 0
+    Dim gravedad As Integer = 4
+    Dim pipes(1) As PictureBox
+    Dim toppipes(1) As PictureBox
+    Dim pipespeed As Single = 9
+    Dim cont As Integer = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+        Timer1.Enabled = True
+        CreatePipes(1)
+        CreateTopPipes(1)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        yspeed += gravedad
+        PictureBox1.Top += yspeed
+        For i = 0 To 1
+            pipes(i).Left -= pipespeed
+            toppipes(i).Left -= pipespeed
+            If Collision(pipes(i), PictureBox1) = True Or Collision(toppipes(i), PictureBox1) = True Then
+                Application.Exit()
+            End If
+            If (PictureBox1.Location.Y >= 284) Or (PictureBox1.Location.Y <= 0) Then
+                Application.Exit()
+            End If
+
+            If pipes(i).Left < 0 Then
+                pipes(i).Left += 400
+                toppipes(i).Left += 400
+                pipes(i).Top = 70 + 290 * Rnd()
+                toppipes(i).Top = pipes(i).Top - 460
+                cont = cont + 1
+                Label2.Text = cont
+                If cont Mod 5 = 0 Then
+                    Me.BackgroundImage = Image.FromFile("C:\Users\gusta\Desktop\Flappy_bird2\Flappy_bird2\sprites\background-night.png")
+                    Me.BackgroundImageLayout = ImageLayout.Stretch
+                End If
+                If cont Mod 10 = 0 Then
+                    Me.BackgroundImage = Image.FromFile("C:\Users\gusta\Desktop\Flappy_bird2\Flappy_bird2\sprites\background-day.png")
+                    Me.BackgroundImageLayout = ImageLayout.Stretch
+                End If
+
+                'If cont <= 9 Then
+                '    PictureBox3.BackgroundImage = Image.FromFile("C:\Users\gusta\Desktop\Flappy_bird2\Flappy_bird2\sprites\" & cont & ".png")
+                '    PictureBox3.BackgroundImageLayout = ImageLayout.Center
+                'End If
+                'If cont > 9 Or cont >= 19 Then
+                '    PictureBox4.Visible = True
+                '    PictureBox3.BackgroundImage = Image.FromFile("C:\Users\gusta\Desktop\Flappy_bird2\Flappy_bird2\sprites\1.png")
+                '    PictureBox3.BackgroundImageLayout = ImageLayout.Center
+                '    PictureBox4.BackgroundImage = Image.FromFile("C:\Users\gusta\Desktop\Flappy_bird2\Flappy_bird2\sprites\" & cont - 10 & ".png")
+                '    PictureBox4.BackgroundImageLayout = ImageLayout.Center
+                'End If
+
+            End If
+        Next
+    End Sub
+
+    Private Sub CreatePipes(ByVal number As Integer)
+        Dim i As Integer = 0
+        For i = 0 To number
+            Dim temp As New PictureBox
+            Me.Controls.Add(temp)
+            temp.Width = 50
+            temp.Height = 350
+            temp.BackgroundImage = Image.FromFile("C:\Users\gusta\Desktop\Flappy_bird2\Flappy_bird2\sprites\pipe-green.png")
+            temp.BackgroundImageLayout = ImageLayout.Stretch
+            temp.BackColor = Color.Transparent
+            temp.Top = 70 + 290 * Rnd()
+            temp.Left = (i * 200) + 300
+            pipes(i) = temp
+            pipes(i).Visible = True
+        Next
+    End Sub
+
+    Private Sub CreateTopPipes(ByVal number As Integer)
+        Dim i As Integer = 0
+        For i = 0 To number
+            Dim temp As New PictureBox
+            Me.Controls.Add(temp)
+            temp.Width = 50
+            temp.Height = 350
+            temp.BackgroundImage = Image.FromFile("C:\Users\gusta\Desktop\Flappy_bird2\Flappy_bird2\sprites\pipe-green1.png")
+            temp.BackgroundImageLayout = ImageLayout.Stretch
+            temp.BackColor = Color.Transparent
+            temp.Top = pipes(i).Top - 460
+            temp.Left = (i * 200) + 300
+            toppipes(i) = temp
+            toppipes(i).Visible = True
+        Next
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If (e.KeyCode = Keys.Space) Then
+            yspeed += -22
+        End If
+    End Sub
+
+    Public Function Collision(ByVal Object1 As Object, ByVal Object2 As Object) As Boolean
+        Dim collided As Boolean = False
+        If Object1.Top + Object1.Height >= Object2.Top And
+           Object2.Top + Object2.Height >= Object1.Top And
+           Object1.Left + Object1.Width >= Object2.left And
+           Object2.Left + Object2.Width >= Object1.left And Object1.visible = True And Object2.visible = True Then
+            collided = True
+        End If
+        Return collided
+    End Function
+End Class
